42fe9155e3031aec08d1fcef096bcff99d5aa9b1
Satyam Jaiswal <satyamjais1193@gmail.com>
2026-09-13 20:27:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (81.34%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,13 +5,19 @@ public:
         int e = nums.size() -1;
         int n = nums.size();
         int ans= -1;
+
+        //handle bounderies casees to avoid additional changes
         if(n == 1) return 0;
+        //when first index is preak
         if(nums[0] > nums[1]) return 0;
+
+        //when last index is peak
         if(nums[n-2] < nums[n-1]) return n-1;
 
         while( s<= e){
             int mid = s + (e-s)/2;
             if((nums[mid] > nums[mid +1]) && ( nums[mid]  > nums[mid-1])){
+                //main codition of finding peak;
                 return mid ;
             }
             else if( nums[mid] < nums[mid +1]){
