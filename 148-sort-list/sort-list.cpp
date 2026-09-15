58f7d45da46fa7ed9cfd3509690d6d354ba019b0
Satyam Jaiswal <satyamjais1193@gmail.com>
2026-09-15 07:40:29 +0530

    Time: 47 ms (35.03%) | Memory: 75.7 MB (31.78%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,24 +10,66 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int> value;
-        ListNode* temp = head;
-        while(temp != nullptr){
-            value.push_back(temp->val);
-            temp = temp-> next;
-        }
+    ListNode* getmiddle(ListNode* head){
+        ListNode* slow = head;
+        //fast is one step ahead of slow to n/2 -1 wala middle
+        //so that we could get direclty next head of next part by slow->next
+        ListNode* fast = head ->next;
 
-        sort(value.begin(), value.end());
-        int i =0;
-        temp = head;
-        while(temp != nullptr){
-            temp->val= value[i];
-            i++; 
-            temp = temp->next;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast ->next->next;
         }
-        return head;
+        return slow;
+    }
 
+    //to merge tow sorted llist
+    ListNode* sortedMerge(ListNode* head1, ListNode* head2) {
+        // code here
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
         
+        while(head1 && head2){
+            if( head1->val <= head2->val){
+                curr->next= head1;
+                head1 = head1->next;
+            }
+            else{
+                curr->next = head2;
+                head2 = head2->next;
+            }
+            curr= curr->next;
+        }
+        
+        //adding left part;
+        if(head1){
+            curr->next = head1;
+        }
+        else{
+            curr->next = head2;
+        }
+        return dummy->next;
+    }
+
+    ListNode* mergesort( ListNode* head){
+        if(!head || !head->next) return head;
+
+        //dividing into two part
+        ListNode* middle = getmiddle(head);
+        ListNode* lefthead = head;
+        ListNode* righthead = middle->next;
+
+        //yaha galti hoti hai--- to seprate left and right part
+        middle->next = nullptr;
+
+        ListNode* leftsort = mergesort(lefthead);
+        ListNode* rightsort = mergesort(righthead);
+
+        return sortedMerge(leftsort, rightsort);
+
+    }
+
+    ListNode* sortList(ListNode* head) {
+        return mergesort(head);
     }
 };
