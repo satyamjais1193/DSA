ce5df3a330a3f6aa9303b8b455bada2f567ca655
Satyam Jaiswal <satyamjais1193@gmail.com>
2026-09-21 22:50:42 +0530

    Time: 35 ms (83.35%) | Memory: 11.1 MB (55.25%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    vector< vector<int>> dp;
-    int solve (int s, int e){
-        if(s>=e) return 0;
+    
+    // int solve (int s, int e){
+    //     if(s>=e) return 0;
 
-        if(dp[s][e] != -1) return dp[s][e];
+    //     if(dp[s][e] != -1) return dp[s][e];
 
-        int ans = 1e9;
-        for(int i = s;i <= e; i++){
-            int rec = i + max(solve( s, i-1 ) , solve( i+1, e));
-            ans = min( rec, ans);
+    //     int ans = 1e9;
+    //     for(int i = s;i <= e; i++){
+    //         int rec = i + max(solve( s, i-1 ) , solve( i+1, e));
+    //         ans = min( rec, ans);
+    //     }
+    //     return dp[s][e] = ans;
+    // }
+
+    int solvetabu(int n){
+        vector< vector<int>> dp( n+2, vector<int>( n+2, 0));
+
+
+        for( int s = n ; s>=1; s--){
+            for(int e = 1; e<=n; e++ ){
+                if(s>=e) continue;
+                int ans = 1e9;
+                for(int i = s;i <= e; i++){
+                    int rec = i + max(dp[s][i-1] , dp[i+1][e]);
+                    ans = min( rec, ans);
+                }
+                dp[s][e] = ans;
+            }
         }
-        return dp[s][e] = ans;
+        return dp[1][n];
     }
     
     int getMoneyAmount(int n) {
-        dp.assign( n+1, vector<int>( n+1, -1));
-        return solve( 1, n);
+        return solvetabu(n);
+        // dp.assign( n+1, vector<int>( n+1, -1));
+        // return solve( 1, n);
     }
 };
