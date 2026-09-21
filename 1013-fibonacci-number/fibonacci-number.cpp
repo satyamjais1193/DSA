8fd026798147ed563b83d67b2bfca08e363e2813
Satyam Jaiswal <satyamjais1193@gmail.com>
2026-09-21 20:16:09 +0530

    Time: 2 ms (77.58%) | Memory: 7.7 MB (88.22%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -22,9 +22,26 @@ public:
         return dp[n];
 
     }
+    int so(int n){
+        // vector<int> dp(n+1, 0);
+
+        int prev  = 1;
+        int nextprev = 0;
+
+        for(int i = 2; i< n+1; i++){
+            int curr = prev + nextprev;
+            nextprev= prev;
+            prev = curr;
+        }
+        return prev;
+
+    }
+
     int fib(int n) {
         if(n ==0) return 0;
         if(n ==1) return 1;
+        return so(n);
+
         return solvetabu(n);
 
         vector<int> dp(n+1, -1);
