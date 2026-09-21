45ac1ed62da599af5d6f945b26d2bd1e21f1a176
Satyam Jaiswal <satyamjais1193@gmail.com>
2026-09-21 20:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (23.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int solvetabu(vector<int>& nums){
+        vector<int>dp(nums.size()+2, 0);
+        int n = nums.size();
+
+        for(int i =n-1; i>=0; i--){
+            int option1= nums[i] + dp[i+2];
+            int option2= dp[i+1];
+
+            dp[i] = max(option1, option2);            
+        }
+        
+
+
+        return dp[0];
+
+    }
+
     int solveusingdp(vector<int>& nums, int i,vector<int>& dp){
         if(i>=nums.size()){
             return 0;
@@ -16,7 +33,11 @@ public:
         return dp[i];
 
     }
+
     int rob(vector<int>& nums) {
+
+        return solvetabu(nums);
+
         vector<int>dp(nums.size(), -1);
         int i=0;
 
